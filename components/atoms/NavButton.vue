d55b64557946f5420378adc7d87ee401b4089846
rurun <docile.notepad.0n@icloud.com>
2026-01-23 10:22:42 +0900

    feat/activeボタンのホバー時に色が薄くなるように変更

--- components/atoms/NavButton.vue
+++ components/atoms/NavButton.vue
@@ -20,7 +20,7 @@
       'flex gap-3 items-center font-extrabold rounded-full focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 transition-colors px-4 py-2 cursor-pointer border-2';
 
     const activeClass = props.active
-      ? 'bg-primary-50 text-primary-500 border-primary-500 focus-visible:ring-primary-500'
+      ? 'bg-primary-50 text-primary-500 border-primary-500 focus-visible:ring-primary-500 hover:opacity-85'
       : 'bg-white text-neutral-600 border-gray-200 focus-visible:ring-gray-200 hover:border-gray-300 hover:bg-neutral-100';
 
     return [base, activeClass].join(' ');
